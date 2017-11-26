322394e9977d17978b4355ff2cc6738f86bbf02f
matt berube <mttbrb@gmail.com>
2017-11-25 23:23:11 -0500

    initial version

---
+++ index.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import WeatherProject from './src/App/WeatherProject';
+
+AppRegistry.registerComponent('weather', () => WeatherProject);
---
+++ src/OpenWeatherMapAPI/open_weather_map.js
@@ -0,0 +1,33 @@
+const WEATHER_API_KEY = "bbeb34ebf60ad50f7893e7440a1e2b0b";
+const API_STEM = "http://api.openweathermap.org/data/2.5/weather?";
+
+function zipUrl(zip) {
+    return `${API_STEM}q={zip}&units=imperial&APPID=${WEATHER_API_KEY}`;
+}
+
+function fetchForecast(zip) {
+    return fetch(zipUrl(zip))
+      .then(response => response.json())
+      .then(responseJSON => {
+          if (responseJSON && 
+              responseJSON.weather && 
+              responseJSON.weather[0]) {
+            return {
+              main: responseJSON.weather[0].main,
+              description: responseJSON.weather[0].description,
+              temp: responseJSON.main.temp
+            }
+          } else {
+            return {
+                main: "error",
+                description: "error goes here",
+                temp: 0
+            }
+          }
+      })
+      .catch(error => {
+          console.error(error);
+      });
+}
+
+export default { fetchForecast: fetchForecast };
